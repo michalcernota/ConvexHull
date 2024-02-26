b37a0677ad86d73164c63aefb77e44661fa7a2cd
Michal Černota <st52494@upce.cz>
2024-02-26 22:48:49 +0100

    Add helper class for handling geometry calculations

---
+++ ConvexHull/GeometryHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace ConvexHull
+{
+    internal static class GeometryHelper
+    {
+
+        public static Orientation GetOrientation(Point p1, Point p2, Point p3)
+        {
+            // 2D cross-product formula
+            int val = (p2.Y - p1.Y) * (p3.X - p2.X) -
+                      (p2.X - p1.X) * (p3.Y - p2.Y);
+
+            switch (val)
+            {
+                case 0: return Orientation.Collinear;
+                case > 0: return Orientation.Clockwise;
+                case < 0: return Orientation.CounterClockwise;
+            }
+        }
+
+        public static List<Point> OrderByPolarAngle(Point startingPoint, IEnumerable<Point> points)
+        {
+            var sortedPoints = points.OrderBy(p =>
+                Math.Atan2(p.Y - startingPoint.Y, p.X - startingPoint.X)).ToList();
+            return sortedPoints;
+        }
+    }
+}
--- ConvexHull/GrahamScan.cs
+++ ConvexHull/GrahamScan.cs
@@ -15,7 +15,7 @@ namespace ConvexHull
                 throw new ArgumentException("At least 3 points are needed in order to construct a convex hull", nameof(points));
 
             var startingPoint = points.OrderBy(p => p.Y).ThenBy(p => p.X).First();
-            var sortedPoints = OrderByPolarAngle(startingPoint, points);
+            var sortedPoints = GeometryHelper.OrderByPolarAngle(startingPoint, points);
             // TODO: remove collinear points by their distance due to P0
 
             var hull = new Stack<Point>();
@@ -24,33 +24,12 @@ namespace ConvexHull
 
             for (int i = 2; i < sortedPoints.Count; i++)
             {
-                while (hull.Count >= 2 && GetOrientation(hull.ElementAt(1), hull.Peek(), sortedPoints[i]) != Orientation.CounterClockwise)
+                while (hull.Count >= 2 && GeometryHelper.GetOrientation(hull.ElementAt(1), hull.Peek(), sortedPoints[i]) != Orientation.CounterClockwise)
                     hull.Pop();
                 hull.Push(sortedPoints[i]);
             }
 
             return hull.ToList();
-        }
-
-        private static List<Point> OrderByPolarAngle(Point startingPoint, IEnumerable<Point> points)
-        {
-            var sortedPoints = points.OrderBy(p =>
-                Math.Atan2(p.Y - startingPoint.Y, p.X - startingPoint.X)).ToList();
-            return sortedPoints;
-        }
-
-        private static Orientation GetOrientation(Point p1, Point p2, Point p3)
-        {
-            // 2D cross-product formula
-            int val = (p2.Y - p1.Y) * (p3.X - p2.X) -
-                      (p2.X - p1.X) * (p3.Y - p2.Y);
-
-            switch (val)
-            {
-                case 0: return Orientation.Collinear;
-                case > 0: return Orientation.Clockwise;
-                case < 0: return Orientation.CounterClockwise;
-            }
         }
     }
 }
